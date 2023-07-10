4abd215e5c9c58efe85aa101c4481378c3f6824a
Adam Balkoev <adam22185515@gmail.com>
2023-07-10 11:54:44 +0300

    Добавил localstorage для темы

--- src/utils/ToggleColorMode.jsx
+++ src/utils/ToggleColorMode.jsx
@@ -1,10 +1,25 @@
-import React, { useState, useMemo, createContext } from 'react';
+import React, {
+  useState, useMemo, createContext, useEffect,
+} from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export const ColorModeContext = createContext();
 
 function ToggleColorMode({ children }) {
   const [mode, setMode] = useState('dark');
+
+  useEffect(() => {
+    const modeFromLocalStorage = localStorage.getItem('theme');
+    if (modeFromLocalStorage) {
+      setMode(modeFromLocalStorage);
+    } else {
+      localStorage.setItem('theme', 'dark');
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('theme', mode);
+  }, [mode]);
 
   const toggleColorMode = () => {
     setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
